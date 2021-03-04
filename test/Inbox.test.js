3f728965c5ebd1e86d0d026109d7d1ded046d661
jimbucktoo <jamesliang.g@gmail.com>
2021-03-03 20:42:08 -0600

    creation of additional mocha tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,8 +1,11 @@
 const assert = require('assert')
 const ganache = require('ganache-cli')
 const Web3 = require('web3')
-const web3 = new Web3(ganache.provider())
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const { interface, bytecode } = require('../compile')
+
+const defaultMessage = 'initial test message'
 
 let accounts
 let inbox
@@ -11,12 +14,24 @@ beforeEach(async () => {
     accounts = await web3.eth.getAccounts()
 
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['hello world'] })
+        .deploy({ data: bytecode, arguments: [ defaultMessage ] })
         .send({ from: accounts[0], gas: '1000000'})
 })
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
         console.log(inbox)
+        assert.ok(inbox.options.address)
+    })
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call()
+        assert.equal(message, defaultMessage)
+    })
+
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('change test message').send({ from: accounts[0] })
+        const message = await inbox.methods.message().call()
+        assert.equal(message, 'change test message')
     })
 })
